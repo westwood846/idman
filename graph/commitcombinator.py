4a1b63197edc5c4e1e9fbde1210d64d8a4e18c9c
turbopope <PapstDonB@Googlemail.com>
2016-05-30 16:06:17 +0200

    Move commitcombinator algorithm into separate module

---
+++ graph/commitcombinator.py
@@ -0,0 +1,10 @@
+import itertools
+
+
+def learn_artifacts(artifact_graph, artifacts):
+    for u, v in itertools.combinations(artifacts, 2):
+        if artifact_graph.has_edge(u, v):
+            artifact_graph[u][v]["label"] += 1
+        else:
+            artifact_graph.add_edge(u, v, label=1)
+    return artifact_graph
