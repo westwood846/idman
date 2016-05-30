c4893b0f4705fc6cef1ff98e88aee034cb3f73a5
turbopope <PapstDonB@Googlemail.com>
2016-05-30 16:43:04 +0200

    Pass whole commit to learning algorithm

--- graph/commitcombinator.py
+++ graph/commitcombinator.py
@@ -1,8 +1,21 @@
 import itertools
 
 
-def learn_artifacts(artifact_graph, artifacts):
-    artifacts = artifacts.values()
+def extract_artifacts(commit):
+    artifacts = {
+        "author_name": commit['author_name'],
+        "author_mail": commit['author_mail'],
+        "committer_mail": commit['committer_mail'],
+        "committer_name": commit['committer_name']
+    }
+    if (commit['signer'] != ""):
+        artifacts["signer"] = commit['signer']
+    if (commit['signer_key'] != ""):
+        artifacts["signer_key"] = commit['signer_key']
+    return artifacts
+
+def learn_commit(artifact_graph, commit):
+    artifacts = extract_artifacts(commit).values()
     for u, v in itertools.combinations(artifacts, 2):
         if artifact_graph.has_edge(u, v):
             artifact_graph[u][v]["label"] += 1
