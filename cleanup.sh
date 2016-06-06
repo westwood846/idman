8a3d18fe394e1ca97a9caa9f6fd47d1dd9d311b3
Michael Monschau <mmonschau@uni-koblenz.de>
2016-06-06 14:53:28 +0200

    added some simple scripts for repo-checkout and reoving binaries

---
+++ cleanup.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+find . | egrep .*\.pyc | xargs rm
---
+++ repos/clone.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#Relevant Java-projects
+git clone "https://github.com/libgdx/libgdx"
+git clone "https://github.com/elastic/elasticsearch"
+git clone "https://github.com/spring-projects/spring-framework"
+#Relevant Django-projects
+git clone "https://github.com/django-oscar/django-oscar"
+git clone "https://github.com/divio/django-cms"
+git clone "https://github.com/django-wiki/django-wiki"
