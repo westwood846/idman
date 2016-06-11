60f38993404e95a8cd0fc75b7b919df642927180
Michael Monschau <github@michael-monschau.de>
2016-06-11 17:56:55 +0200

    implemented author/committer/singer-only processing

--- lib/graph/util/filters.py
+++ lib/graph/util/filters.py
@@ -20,6 +20,12 @@ def remove_certain_keys(dict,keyword,strict=False):
     else:
         return filter_keys(dict, lambda x:  keyword not in x)
 
+# gets all keys from the dictionary wich contain a certain keyword (in strict mode equality is checked)
+def get_certain_keys(dict,keyword,strict=False):
+    if strict:
+        return filter_keys(dict, lambda x: keyword == x)
+    else:
+        return filter_keys(dict, lambda x:  keyword in x)
 
 def remove_author(commit):
     return remove_certain_keys(commit,"author")
@@ -31,3 +37,13 @@ def remove_committer(commit):
 
 def remove_signer(commit):
     return remove_certain_keys(commit,"signer")
+
+
+# removes falsy values from list
+def clean_list(l):
+    return [x for x in l if x]
+
+
+#  removes all elements from list for which function does not return true
+def filter_list(list,function):
+    return [v for v in list if function(v)]
